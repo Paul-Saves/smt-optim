ba9d5f600854179780d29fddb0025d4134d737ba
Antoine Maugras <amaugras75@gmail.com>
2026-06-02 17:22:59 -0400

    Added Pymoo comparison

--- src/biego_strategy_test.py
+++ src/biego_strategy_test.py
@@ -12,6 +12,14 @@ from smt_optim.benchmarks.registry import list_problems
 
 import matplotlib.pyplot as plt
 
+from pymoo.core.problem import ElementwiseProblem
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.operators.crossover.sbx import SBX
+from pymoo.operators.mutation.pm import PM
+from pymoo.operators.sampling.rnd import FloatRandomSampling
+from pymoo.optimize import minimize
+from pymoo.termination import get_termination
+
 bounds=np.array([[-4, 4],[-4,4]])
 
 n_accuracy=1000
@@ -97,23 +105,67 @@ def run_benchmark(bproblem):
     state = driver.optimize()
     return state
 
+class PymooProblem(ElementwiseProblem):
+
+    def __init__(self,bprob):
+        self.prob=bprob
+        super().__init__(n_var=self.prob.num_dim,
+                         n_obj=self.prob.num_obj,
+                         xl=np.array([coord_bounds[0] for coord_bounds in self.prob.bounds]),
+                         xu=np.array([coord_bounds[1] for coord_bounds in self.prob.bounds]))
+
+    def _evaluate(self, x, out, *args, **kwargs):
+        f1 = self.prob.objective[0](x)
+        f2 = self.prob.objective[1](x)
+
+        out["F"] = [f1, f2]
+
+def run_benchmark_pymoo(bproblem):
+    pymoo_problem=PymooProblem(bproblem)
+    algorithm = NSGA2(
+        pop_size=100,
+        n_offsprings=25,
+        sampling=FloatRandomSampling(),
+        crossover=SBX(prob=0.9, eta=15),
+        mutation=PM(eta=20),
+        eliminate_duplicates=True
+    )
+    termination = get_termination("n_gen", 1000)
+    res = minimize(pymoo_problem,
+                algorithm,
+                termination,
+                seed=1,
+                save_history=True,
+                verbose=True)
+
+    X = res.X
+    F = res.F
+    return (X,F)
 
 data=[]
 
 for bproblem in L:
     state=run_benchmark(bproblem)
-    data.append((bproblem.name,state))
+    X,F=run_benchmark_pymoo(bproblem)
+    data.append((bproblem.name,state,X,F))
+
 
 _,axs=plt.subplots(3,3)
 
 for i in range(3):
     for j in range(3):
         index = i*3+j
+        ax=axs[i][j]
+
         D,Y=get_DoE(data[index][1])
         pareto_points = [(D[i],Y[i]) for i in ParetoFront(D,Y)]
-        
-        ax=axs[i][j]
         ax.scatter([p[1][0] for p in pareto_points],[p[1][1] for p in pareto_points])
+
+        X,F=data[index][2],data[index][3]
+        pareto_points_pymoo = [(X[i],F[i]) for i in ParetoFront(X,F)]
+        ax.scatter([p[1][0] for p in pareto_points_pymoo],[p[1][1] for p in pareto_points_pymoo])
+
         ax.title.set_text(data[index][0])
 
 plt.show()
+
